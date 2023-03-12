2ac9911c6043c2f5a854182f173e559a190f9fc4
unknown <shyamsharma.sharma797@gmail.com>
2023-03-12 09:30:09 -0400

    application deployed

--- src/main/java/com/example/demo/RestapiApplication.java
+++ src/main/java/com/example/demo/RestapiApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.example.controller.TodoController;
+import com.example.demo.repository.ToDoRepository;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = TodoController.class)
+@EnableMongoRepositories(basePackageClasses = ToDoRepository.class)
+
 
 public class RestapiApplication {
 
