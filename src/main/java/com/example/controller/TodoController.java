97be6142f0d3e93de3f6a65730dd41c71be3b1d8
unknown <shyamsharma.sharma797@gmail.com>
2023-03-12 09:22:44 -0400

    application deployed

--- src/main/java/com/example/controller/TodoController.java
+++ src/main/java/com/example/controller/TodoController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.ToDoRepository;
 import com.example.model.Todo;
 
 @RestController
+@RequestMapping("/api/v1")
 public class TodoController {
 	
 	@Autowired
