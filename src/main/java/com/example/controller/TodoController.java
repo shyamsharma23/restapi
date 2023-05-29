46379cb9cecf064eb399cb2db9afddef5892326e
unknown <shyamsharma.sharma797@gmail.com>
2023-05-29 09:20:49 -0400

    Applied Cors modification

--- src/main/java/com/example/controller/TodoController.java
+++ src/main/java/com/example/controller/TodoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.repository.ToDoRepository;
 import com.example.model.Todo;
 
+@CrossOrigin(origins = "http://127.0.0.1:5173/")
 @RestController
 @RequestMapping("/api/v1")
 public class TodoController {
@@ -73,6 +75,7 @@ public class TodoController {
 		}
 	}
 	
+	@CrossOrigin(origins = "http://127.0.0.1:5173/")
 	@DeleteMapping("todos/{id}")
 	public ResponseEntity<?> deleteTodoById(@PathVariable("id") String id){
 		Optional<Todo> item = repo.findById(id);
