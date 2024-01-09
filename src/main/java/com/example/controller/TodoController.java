538e4948f9def00f8bea1b116129f0efe65d6154
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2024-01-09 22:15:54 +0530

    Commit

--- src/main/java/com/example/controller/TodoController.java
+++ src/main/java/com/example/controller/TodoController.java
@@ -24,64 +24,63 @@ import com.example.model.Todo;
 @RestController
 @RequestMapping("/api/v1")
 public class TodoController {
-	
+
 	@Autowired
 	private ToDoRepository repo;
-	
+
 	@GetMapping("/todos")
 	public ResponseEntity<?> getAllTodos() {
 		List<Todo> todos = repo.findAll();
-		if(todos.size()>0) {
+		if (todos.size() > 0) {
 			return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
-		}
-		else {
+		} else {
 			return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
 		}
-		
+
 	}
-	
+
 	@PostMapping("/todos")
-	public ResponseEntity<?> createTodo(@RequestBody Todo todos){
+	public ResponseEntity<?> createTodo(@RequestBody Todo todos) {
 		todos.setCreatedAt(new Date(System.currentTimeMillis()));
 		repo.save(todos);
 		return new ResponseEntity<Todo>(todos, HttpStatus.OK);
 	}
-	
+	// My comment for Jenkins
+
 	@GetMapping("/todos/{id}")
-	public ResponseEntity<?> getTodo(@PathVariable("id") String id){
+	public ResponseEntity<?> getTodo(@PathVariable("id") String id) {
 		Optional<Todo> item = repo.findById(id);
-		if(item.isPresent()) {
+		if (item.isPresent()) {
 			return new ResponseEntity<>(item.get(), HttpStatus.OK);
-		}
-		else {
+		} else {
 			return new ResponseEntity<>("Todo not found", HttpStatus.NOT_FOUND);
 		}
 	}
-	
-	//Controller to accept put request
-	
+
+	// Controller to accept put request
+
 	@PutMapping("/todos/{id}")
-	public ResponseEntity<?> getTodo(@RequestBody Todo todo, @PathVariable("id") String id){
+	public ResponseEntity<?> getTodo(@RequestBody Todo todo, @PathVariable("id") String id) {
 		Optional<Todo> item = repo.findById(id);
-		if(item.isPresent()) {
+		if (item.isPresent()) {
 			Todo todoToSave = item.get();
 			todoToSave.setCompleted(todo.getCompleted() != null ? todo.getCompleted() : todoToSave.getCompleted());
 			todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
-			todoToSave.setTodo(todo.getTodo() !=null ? todo.getTodo() : todoToSave.getTodo());
-			todoToSave.setDescription(todo.getDescription() != null ? todo.getDescription(): todoToSave.getDescription());
+			todoToSave.setTodo(todo.getTodo() != null ? todo.getTodo() : todoToSave.getTodo());
+			todoToSave.setDescription(
+					todo.getDescription() != null ? todo.getDescription() : todoToSave.getDescription());
 			repo.save(todoToSave);
 			return new ResponseEntity<Todo>(todoToSave, HttpStatus.OK);
-		}
-		else {
+		} else {
 			return new ResponseEntity<>("Todo you want to uodate not found", HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@CrossOrigin
 	@DeleteMapping("todos/{id}")
-	public ResponseEntity<?> deleteTodoById(@PathVariable("id") String id){
+	public ResponseEntity<?> deleteTodoById(@PathVariable("id") String id) {
 		Optional<Todo> item = repo.findById(id);
-		if(item.isPresent()) {
+		if (item.isPresent()) {
 			Todo todoToDelete = item.get();
 			repo.delete(todoToDelete);
 			return new ResponseEntity<>("Todo has been deleted successfully", HttpStatus.OK);
